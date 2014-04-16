fc7ca0c62947bd777bdab058dcc2845210f778e8
Alexey Chernousov <iamsaint@yandex.ru>
2014-04-17 03:35:13 +0400

    db load field

--- server.rb
+++ server.rb
@@ -1,25 +1,65 @@
-puts 'Debug Server v 0.01'
-
-policyInfo = '&lt;?xml version="1.0"?&gt;'
-policyInfo += '&lt;cross-domain-policy&gt;'
-policyInfo += '&lt;allow-access-from domain="*" to-ports="4444,80" /&gt;'
-policyInfo += "&lt;/cross-domain-policy&gt;\0"
-
 require 'socket'
-server = TCPServer.new('localhost', 4444);
+require 'pg'
+require 'xmlsimple'
 
-while(session = server.accept)
+@conn = PGconn.new('localhost', 5432, '','', 'test', 'test', 'test');
+@fieldId = 0;
+def sendHeaders(ses)
+  policyInfo = '&lt;?xml version="1.0"?&gt;'
+  policyInfo += '&lt;cross-domain-policy&gt;'
+  policyInfo += '&lt;allow-access-from domain="*" to-ports="4444,80" /&gt;'
+  policyInfo += "&lt;/cross-domain-policy&gt;\0"
+  ses.print policyInfo
+end
 
-  Thread.new(session) do |s|
+def response(ses, resp)
+  ses.print resp+"\0"
+end
 
-    s.print policyInfo
+def loadField()
 
-    while(msg = s.gets)
-      msg.strip!
-      puts msg
-      s.print msg
+  str = '<root><action>load</action><field '
+  @conn.exec_params( "SELECT * FROM fields where id=$1", [@fieldId] ) do |result|
+    str += 'coins="'+result[0]['coins']+'" population="'+result[0]['population']+'" power="'+result[0]['power']+'">'
+  end
+
+  @conn.exec_params( "SELECT * FROM gameobjects where fieldid=$1", [@fieldId] ) do |result|
+    result.each do |row|
+     str += '<'+row['type']+' x="'+row['x']+'" y="'+row['y']+'" time="'+row['time']+'" id="'+row['id']+'" />'
     end
 
   end
 
+  str += '</field></root>'
+  puts str
+  return str
+  #return '<root><action>load</action><field coins="'+res['coins']+'" population="'+res['population']+'" power="'+res['power']+'">'+'</field></root>';
+end
+
+server = TCPServer.new('localhost', 4444);
+fieldId = 0
+while(session = server.accept)
+  Thread.new(session) do |s|
+    sendHeaders(s)
+    while(msg = s.gets)
+      begin
+        ref = XmlSimple.xml_in(msg, {'ForceArray' => false })
+        resp = ''
+
+        if ref['action']=='auth'
+          @fieldId = ref['fieldId']
+        elsif ref['action']=='load'
+          resp = loadField()
+        else
+          puts ref['action']
+        end
+
+        if resp != ''
+          response(s, resp);
+        end
+      rescue Exception => e
+        puts e
+      end
+    end
+  end
 end
